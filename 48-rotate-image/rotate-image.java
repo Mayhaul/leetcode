b9f8e4bf78f3c1b61fe5dc59c8aa6a3d48e591a8
Mehul Verma <mehulv2k6@gmail.com>
2026-09-16 21:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (16.61%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        int[][] New = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                New[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = New[i][j];
+            }
+        }
+
+    }
+}
+
+// Brute force
