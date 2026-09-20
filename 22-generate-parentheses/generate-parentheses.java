1a424c5f0b70aee4cc042124e33542ff987511f0
Mehul Verma <mehulv2k6@gmail.com>
2026-09-20 15:03:05 +0530

    Time: 1 ms (85.44%) | Memory: 44.4 MB (79.87%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,9 +1,8 @@
 class Solution {
     public void helper(int n, int open, int close, StringBuilder str, List<String> ans) {
         if (str.length() == 2 * n) {
-            if (validate(str)) {
-                ans.add(str.toString());
-            }
+
+            ans.add(str.toString());
             return;
         }
         if (open < n) {
@@ -15,35 +14,6 @@ class Solution {
             str.deleteCharAt(str.length() - 1);
         }
 
-    }
-
-    public boolean validate(StringBuilder str) {
-        if (str == null) {
-            return false;
-        }
-
-        Deque<Character> stack = new ArrayDeque<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } else if (ch == ')' || ch == '}' || ch == ']') {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-
-                char top = stack.pop();
-                if ((ch == ')' && top != '(') ||
-                        (ch == '}' && top != '{') ||
-                        (ch == ']' && top != '[')) {
-                    return false;
-                }
-            }
-        }
-
-        return stack.isEmpty();
     }
 
     public List<String> generateParenthesis(int n) {
